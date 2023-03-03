00be022de80ce977622f99ebe4e8722474d171d7
bird <mbird3@gmail.com>
2023-03-03 15:41:00 -0800

    work on cast list

--- movie_b/movie_buff/02_queries.rb
+++ movie_b/movie_buff/02_queries.rb
@@ -7,13 +7,20 @@ end
 
 def bad_years
   # List the years in which no movie with a rating above 8 was released.
-  
+  Movie
+  .group(:yr)
+  .having("max(score) <= 8")
+  .pluck(:yr)
 end
 
 def cast_list(title)
   # List all the actors for a particular movie, given the title.
   # Sort the results by starring order (ord). Show the actor id and name.
-  
+  Actor
+  .joins(:movies)
+  .where("movies.title = ?", title)
+  # .order ("castings.ord DESC")
+  .pluck(:id, :name)
 end
 
 def vanity_projects
