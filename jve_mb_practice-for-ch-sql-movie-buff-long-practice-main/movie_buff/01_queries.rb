89fb08049137668e8d82b94ad8e10643213d9b0b
Josh van Eyken <josh.vaneyken@gmail.com>
2023-03-03 14:15:29 -0800

    first problem

--- jve_mb_practice-for-ch-sql-movie-buff-long-practice-main/movie_buff/01_queries.rb
+++ jve_mb_practice-for-ch-sql-movie-buff-long-practice-main/movie_buff/01_queries.rb
@@ -6,7 +6,8 @@ def it_was_ok
   # You can use ranges (a..b) inside a where method.
   #
   # Find the id, title, and score of all movies with scores between 2 and 3.
-
+  Movie.where(score: (2..3)).select(:id, :title, :score)
+  # Movie.all
 end
 
 def harrison_ford
@@ -20,7 +21,9 @@ def harrison_ford
   #
   # Find the id and title of all movies in which Harrison Ford appeared but not
   # as a lead actor.
-
+Actor
+  .joins(:movies).where("actors.name = 'Harrison Ford' AND castings.ord <> 1")
+  .select(:id)
 end
 
 def biggest_cast
